c8bea2da91d2078d6a2db4d576880a023662e21b
Diana Bowler <diana.e.bowler@gmail.com>
2022-09-26 13:45:44 +0100

    first play to visualize the correlations

---
+++ 07_biodiversity_response_correlations.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(ggcorrplot)
+
+resFolders <- "data/cleaned_data"
+
+#read in biodiversity data
+df <- readRDS(paste(resFolders,"data_richness.RDS", sep="/"))
+
+
+#subset to response metrics
+df <- df %>%
+        dplyr::select(contains("richness"),
+                      contains("shannon"),
+                      contains("Biomass"),
+                      contains("n_reads")) %>%
+        select(-est_richness_model, -biomassUncertainty)
+
+#sep for Year and Time_band eventually
+
+#get and plot correlation matrix
+corr <- round(cor(df, use="pairwise.complete.obs"), 2)
+ggcorrplot(corr, hc.order = TRUE, type = "lower",lab = TRUE,
+           outline.col = "white",
+           ggtheme = ggplot2::theme_minimal,
+           colors = c("#6D9EC1", "white", "#E46726"))

1ebada2004d7eb457dd0c0317d8ad687ef56c128
Cecilie Skræp Svenningsen <psl603@ku.dk>
2022-10-11 11:53:05 +0200

    plot output of richness variables comparison, add standardised time fields to the data

--- reports/06_Richness_and_diversity_response_variables.R
+++ reports/06_Richness_and_diversity_response_variables.R
@@ -4,6 +4,7 @@
 library(tidyverse)
 library(readr)
 library(data.table)
+library(lubridate)
 
 # stats
 library(vegan)
@@ -20,6 +21,7 @@ library(sjPlot)
 library(ggplot2)
 library(cowplot)
 library(ggpubr)
+library(scales)
 
 #### load data ####
 
@@ -66,7 +68,8 @@ nrow(unique(taxonomy[,c('scientificName','genus')]))
 n_distinct(taxonomy$scientificName) # no difference between the two, so no BINs are assigned to the same family
 
 asvs_allBINs <- asvs # keep the dataframe
-forsubset <- taxonomy_allBINs %>% select(occurrenceId, scientificName) # to keep the samples, we need to include BINs temporarily in the AVS table
+forsubset <- taxonomy %>% select(occurrenceId, scientificName) # to keep the samples, we need to include BINs temporarily in the AVS table
+
 asvs <- asvs_allBINs %>% rownames_to_column("occurrenceId") 
 asvs <- left_join(asvs, forsubset, by = "occurrenceId") # include BINs in the asv table
 
@@ -88,7 +91,7 @@ taxonomy_allBINs <- taxonomy
 taxonomy <- taxonomy_allmatches %>% filter(occurrenceId %in% keep)
 
 min(rowSums(asvs)) # some very low read sequences in there...
-min(colSums(test)) 
+min(colSums(asvs)) 
 
 # How many unique in each taxonomic level? 
 
@@ -101,14 +104,14 @@ taxonomy %>%
   group_by(order) %>%
   drop_na(order) %>% 
   summarise(n = n()) %>%
-  mutate(Freq = n/sum(n)) %>% 
+  mutate(Freq = n/sum(n)*100) %>% 
   arrange(desc(n))
 
 taxonomy %>%
   group_by(order) %>%
   drop_na(order) %>% 
   summarise(bins = n_distinct(scientificName)) %>% 
-  mutate(freq = bins/sum(bins)) %>% 
+  mutate(freq = bins/sum(bins)*100) %>% 
   arrange(desc(bins))
 
 # family
@@ -117,15 +120,15 @@ taxonomy %>% drop_na(family) %>% summarise_all(n_distinct)
 taxonomy %>%
   group_by(family) %>%
   drop_na(family) %>% 
-  summarise(n = n()) %>%
-  mutate(Freq = n/sum(n)) %>% 
-  arrange(desc(n))
+  summarise(n_seq = n()) %>%
+  mutate(freq = n_seq/sum(n_seq)*100) %>% 
+  arrange(desc(n_seq))
 
 taxonomy %>%
   group_by(family) %>%
   drop_na(family) %>% 
   summarise(bins = n_distinct(scientificName)) %>% 
-  mutate(freq = bins/sum(bins)) %>% 
+  mutate(freq = bins/sum(bins)*100) %>% 
   arrange(desc(bins))
 
 # genus
@@ -260,6 +263,9 @@ est_richness %>%
 data_richness <- inner_join(data, richness, by = "SampleID") %>% 
   inner_join(., shannon, by = "SampleID") %>% inner_join(., est_richness, by = "SampleID")
 
+setdiff(data$SampleID, data_richness$SampleID) # in data but not in the richness data
+setdiff(names(asvs), data_richness$SampleID) # in asvs but not in the richness data
+
 # make the different richness estimate names more clear
 data_richness <- data_richness %>%
   rename(richness_rarefied = richness,
@@ -271,9 +277,54 @@ data_richness %>%
   summarize(mean(diff), sd(diff)) # so mean richness seems a bit lower for rarefied richness
 
 # visualise it
-data_richness %>% 
-  select(SampleID, n_reads, obs_richness, richness_rarefied, richness_est) %>% 
-  pivot_longer(-c(SampleID, n_reads)) %>% 
-  ggplot(aes(x = n_reads, y = value, color=name)) + geom_point() + geom_smooth()
+png(file="plots/richness_compared.png",
+     width=2000, height=1200, res=300)
+data_richness %>%
+  select(SampleID,
+         n_reads,
+         obs_richness,
+         richness_rarefied,
+         richness_est) %>%
+  pivot_longer(-c(SampleID, n_reads)) %>%
+  ggplot(aes(x = n_reads, y = value, color = name)) + geom_point() + geom_smooth() +
+  xlab("Number of reads") + ylab("Richness") +
+  scale_color_hue(labels = c("Observed", "Estimated", "Rarefied")) + theme_minimal_grid() + scale_x_continuous(labels = label_number_si(), breaks = scales::pretty_breaks(n = 10)) + theme(legend.title = element_blank()) 
+dev.off()
+
+### add standardised time ####
+
+data_richness <- data_richness %>% 
+  filter(!is.na(Time_band)) # DK: a few samples were not sampled within time bands
+
+#sort time data to standard each around the time band
+data_richness$numberTime <- as.numeric(hms(data_richness$StartTime))
+
+#testing 
+rnorm(10)
+
+#centering
+data_richness$cyDay <- data_richness$yDay - median(data_richness$yDay)
+#data_richness$cStops <- log(data_richness$stops+1) - median(log(data_richness$stops+1))
+data_richness$cTL <- log(data_richness$Num_trafficLights+1) - median(log(data_richness$Num_trafficLights+1))
+
+#transform to minutes
+data_richness$numberTime <- data_richness$numberTime/60 
+
+#centre time around each time band
+middayMean <- median(data_richness$numberTime[data_richness$Time_band=="midday"],na.rm=T)
+eveningMean <- median(data_richness$numberTime[data_richness$Time_band=="evening"],na.rm=T)
+
+data_richness$cnumberTime <- NA
+data_richness$cnumberTime[data_richness$Time_band=="midday"] <- data_richness$numberTime[data_richness$Time_band=="midday"] -middayMean
+data_richness$cnumberTime[data_richness$Time_band=="evening"] <- data_richness$numberTime[data_richness$Time_band=="evening"] -eveningMean
+
+#set missing values to mean of time band
+data_richness$cnumberTime[is.na(data_richness$cnumberTime)]<- 0
+
+qplot(x=Agriculture_1000, y=richness_est, data = data_richness,colour=Urban_1000, geom="point", )
+
+# there are still samples without DNA conc., quadrant ID and without end time. But this I minor issues and will not have an impact on the analysis
 
 saveRDS(data_richness, file = "data/cleaned_data/data_richness_BINs.RDS") # the saved output without the BIN extension is for unique sequences 
+saveRDS(taxonomy, file = "data/cleaned_data/taxonomy_BIN_combo.RDS")
+saveRDS(asvs, file = "data/cleaned_data/asvs_BIN_combo.RDS")
